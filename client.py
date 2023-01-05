5e342e7c033837bcf42d8e0ca5e4e65e5acf5fbf
Raphael Tolila <raphael.tolila@gmail.com>
2023-01-06 00:00:37 +0100

    client relier aux servers

--- client.py
+++ client.py
@@ -1,22 +1,16 @@
 from listen import audio
 from server import get_weather
 import requests
+headers = {'Content-Type': 'application/json'}
 
-
-URL = "http://185.142.53.175:5000/"
 def run():
     client_on = True
     while client_on:
-        audio()
-        if audio() == "okay jarvis":
-            audio()
-            if audio() == "weather in":
-                city = audio()
-                x = audio().split()
-
-                # faudra modifier audio() quand on aura fait la classe pour listen et ce  sera plussimple de l'appeler
-                client_on = False
-            if audio() == "stop":
-                client_on = False
+        if audio() == "weather":
+            response = requests.get(f"http://185.142.53.175:5000/get_weather/{audio()}")
+            print(response.json())
+        
+        if audio() == "stop":
+            client_on = False
 
 run()
--- server.py
+++ server.py
@@ -2,14 +2,15 @@ from flask import Flask
 import requests
 import json
 from functions import *
+
 app = Flask(__name__)
 
 list_of_functions = ["get_weather"]
 @app.route("/<function>/<param1>")
 def action(function, param1):
     if str(function) in list_of_functions:
-
-        return eval(function + "(param1)")
+        answer = eval(function + "(param1)")
+        return answer
 
     else:
         return "je ne sais pas encore faire ça"
@@ -17,4 +18,4 @@ def action(function, param1):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
