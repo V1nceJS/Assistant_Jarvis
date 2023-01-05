4b8049972360a652985a5bc9748de749ca112cf7
Raphael Tolila <raphael.tolila@gmail.com>
2023-01-05 21:52:02 +0100

    add stop and remove face.py

--- client.py
+++ client.py
@@ -1,11 +1,9 @@
 from listen import audio
 from server import get_weather
-from Face import face
 
 
 def run():
     client_on = True
-    face()
     while client_on:
         audio()
 
@@ -14,6 +12,7 @@ def run():
             print(get_weather(ville))
             # faudra modifier audio() quand on aura fait la classe pour listen et ce  sera plussimple de l'appeler
             client_on = False
-
+        if audio() == "stop":
+            client_on = False
 
 run()
